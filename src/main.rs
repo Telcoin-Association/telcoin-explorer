e8e05cc59617976dbd88a82eba4329902338aa0d
Nick - Bluelights <blu3chipsx@gmail.com>
2026-05-10 12:02:53 +0100

    v0.1.16: fix sha3 injection using js_sys eval

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,7 @@ use router::Route;
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
     // Inject keccak256 library for ABI selector computation
-    if let Some(window) = web_sys::window() {
-        if let Some(doc) = window.document() {
-            if let Some(head) = doc.head() {
-                if let Ok(script) = doc.create_element("script") {
-                    script.set_attribute("src", "https://cdnjs.cloudflare.com/ajax/libs/js-sha3/0.9.3/sha3.min.js").ok();
-                    head.append_child(&script).ok();
-                }
-            }
-        }
-    }
+    let _ = js_sys::eval("var s=document.createElement('script');s.src='https://cdnjs.cloudflare.com/ajax/libs/js-sha3/0.9.3/sha3.min.js';document.head.appendChild(s);");
     dioxus::launch(App);
 }
 
